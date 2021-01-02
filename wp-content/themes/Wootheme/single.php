4be9025ff442a36469813b7ccd33d661ec6f2fc6
iforyourajiv <sddrajiv@gmail.com>
2021-01-02 19:03:24 +0530

    task2(custome meta box,custome option creation)

--- wp-content/themes/Wootheme/single.php
+++ wp-content/themes/Wootheme/single.php
@@ -10,6 +10,9 @@
                     while (have_posts()) : 
                     the_post(); ?>
                         <h2 class="title"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
+                        <h3>Color:<?php 
+                        $id=get_the_ID();
+                        echo  get_post_meta($id,'ced_meta_box_metakey',true); ?></h3>
                         <p class="meta">Posted By <a><?php the_author_link() ?></a> On <?php echo get_the_date() ?> <a href="#" class="comments">Comments (64)</a> &nbsp;&bull;&nbsp; <a href="#" class="permalink">Full article</a></p>
                         <div class="entry">
                             <?php the_content(); ?>
